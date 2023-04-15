bc5e8c83e5a22195c46e2182c0b205ae0f67bce9
DarxyDev <damongarvin@gmail.com>
2023-04-14 20:57:49 -0400

    added gameboardFactory and tests

--- src/script/obj-factories.js
+++ src/script/obj-factories.js
@@ -1,17 +1,74 @@
-function shipFactory(name, length){
+function shipFactory(name, length) {
     const _name = name;
     const _length = length;
     let _hits = 0;
-    let ship = {
-        get:{
-            name: ()=>{return _name},
-            length: ()=>{return _length},
-            hits:()=>{return _hits}
+
+    const ship = {
+        get: {
+            name: () => { return _name },
+            length: () => { return _length }
         },
-        hit:()=>{_hits++;},
-        isSunk:()=>{return (_hits >= _length)}
+        hit: () => { _hits++; },
+        isSunk: () => { return (_hits >= _length) }
     }
     return ship;
 }
 
-export {shipFactory};
+function gameboardFactory(width = 7, height = 7) {
+    const boardSize = width * height;
+    let _shipsRemaining = 0;
+    const _boardArray = [];
+    for (let i = 0; i < boardSize; i++) _boardArray.push(false);
+    const _hitArray = [];
+    for (let i = 0; i < boardSize; i++) _hitArray.push(false);
+
+    const gameboard = {
+        get: {
+            shipsRemaining: () => { return _shipsRemaining }
+        },
+        placeShip: (ship, coord, rotated) => {
+            for (let i = 0; i < ship.get.length(); i++) {
+                let j = rotated ?
+                    _get2DIndex(coord[0], coord[1] + i) :
+                    _get2DIndex(coord[0] + i, coord[1]);
+                if (_boardArray[j]) return false;
+            }
+            for (let i = 0; i < ship.get.length(); i++) {
+                let j = rotated ?
+                    _get2DIndex(coord[0], coord[1] + i) :
+                    _get2DIndex(coord[0] + i, coord[1]);
+                _boardArray[j] = ship;
+            }
+
+            _shipsRemaining++;
+            return true;
+        },
+        receiveAttack: (coord) => {
+            const i = _get2DIndex(coord);
+
+            if (_hitArray[i]) return false;
+            _hitArray[i] = true;
+
+            const ship = _boardArray[i];
+            if(!ship) return 'miss';
+            ship.hit();
+            if(ship.isSunk()){
+                _shipsRemaining--;
+                return 'sunk';
+            }
+            return 'hit';
+        },
+        isGameOver: ()=>{return _shipsRemaining <= 0}
+    }
+    return gameboard;
+    //private
+    function _get2DIndex(a, b) {
+        if (typeof (a) === typeof ([]) &&
+            (b === undefined)) {
+            return a[0] * width + a[1];
+        }
+        else return a * width + b;
+    }
+}
+
+export { shipFactory, gameboardFactory };
--- src/script/test/obj-factories.test.js
+++ src/script/test/obj-factories.test.js
@@ -1,14 +1,31 @@
-import { shipFactory } from "../obj-factories";
+import { shipFactory, gameboardFactory } from "../obj-factories";
 
-test('ShipFactory returns ship info correctly',()=>{
-    const testShip = shipFactory('testName',3);
-    expect(testShip.get.name()).toBe("testName");
-    expect(testShip.get.length()).toBe(3);
-    expect(testShip.get.hits()).toBe(0);
-    testShip.hit();
-    expect(testShip.get.hits()).toBe(1);
-    testShip.hit();
-    testShip.hit();
-    expect(testShip.get.hits()).toBe(3);
-    expect(testShip.isSunk()).toBe(true);
+test('ShipFactory',()=>{
+    const ship = shipFactory('testName',3);
+    expect(ship.get.name()).toBe("testName");
+    expect(ship.get.length()).toBe(3);
+
+    ship.hit();
+    ship.hit();
+    ship.hit();
+    expect(ship.isSunk()).toBe(true);
+})
+
+test('gameboardFactory',()=>{
+    const gameboard = gameboardFactory(7,7);
+    const ship = shipFactory('testName',3);
+
+    expect(gameboard.placeShip(ship,[0,0],false)).toBe(true);
+    expect(gameboard.placeShip(ship,[0,0],false)).toBe(false);
+
+    expect(gameboard.get.shipsRemaining()).toBe(1);
+    expect(gameboard.receiveAttack([0,0])).toBe('hit');
+    expect(gameboard.receiveAttack([0,0])).toBeFalsy();
+    expect(gameboard.receiveAttack([0,4])).toBe('miss');
+    expect(gameboard.receiveAttack([1,0])).toBe('hit');
+    expect(gameboard.receiveAttack([2,0])).toBe('sunk');
+    expect(gameboard.get.shipsRemaining()).toBe(0);
+    expect(gameboard.isGameOver()).toBeTruthy();
+
+
 })
