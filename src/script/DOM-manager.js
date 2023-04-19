3614c0df7230ec14dd032d700879f3961c79026f
DarxyDev <damongarvin@gmail.com>
2023-04-19 14:35:36 -0400

    added normalize.css, style.css and linked them

---
+++ src/script/DOM-manager.js
@@ -0,0 +1,25 @@
+const domManager = (() => {
+    function setElementAspectRatio(element, ratio = 1) {
+        const parent = element.parentNode;
+        window.addEventListener('resize', _onResize);
+        _onResize();
+        function _onResize(e) {
+            const width = parent.offsetWidth;
+            const height = parent.offsetHeight;
+
+            let newSize;
+            if (height > width) newSize = width;
+            else newSize = height;
+
+            let newWidth = newSize;
+            let newHeight = newSize;
+            if (ratio > 1) newHeight *= (1 / ratio);
+            else newWidth *= ratio;
+
+            element.style.width = newWidth + 'px';
+            element.style.height = newHeight + 'px';
+        }
+    }
+    return { setElementAspectRatio };
+})();
+export default domManager;
