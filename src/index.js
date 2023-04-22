6b678461e253e15dc884c87c4331248314408fbd
DarxyDev <damongarvin@gmail.com>
2023-04-22 17:32:35 -0400

    set references to player-select-menu elements

--- src/index.js
+++ src/index.js
@@ -3,8 +3,11 @@ import './style/style.css';
 import config from './script/config'
 import domManager from './script/DOM-manager';
 
+
 const ref = domManager.getReferences();
 let sizeRatio = config.get.gameboardSize.width() / config.get.gameboardSize.height();
+
+
 
 // domManager.createGameboards(sizeRatio);
 // domManager.switchToPlayer1();
--- src/script/DOM-manager.js
+++ src/script/DOM-manager.js
@@ -1,18 +1,20 @@
 const domManager = (() => {
     const _references = {
-        game: {
-            sections: {
-                left: document.getElementById('left-section'),
-                right: document.getElementById('right-section'),
-                top: document.getElementById('top-section')
-            },
-            p1: {
-                playerBoard: undefined,
-                enemyBoard: undefined
-            },
-            p2: {
-                playerBoard: undefined,
-                enemyBoard: undefined
+        menus: {
+            playerSelect: {
+                player1:{
+                    typeBtn: document.getElementById('player1-type-btn'),
+                    typeHeader: document.getElementById('player1-type-header'),
+                    nameInput: document.getElementById('player1-name-input')
+                },
+                player2:{
+                    typeBtn: document.getElementById('player2-type-btn'),
+                    typeHeader: document.getElementById('player2-type-header'),
+                    nameInput: document.getElementById('player2-name-input')
+                },
+                gameboardSettings: {
+                    startGameBtn: document.getElementById('start-game-btn')
+                }
             },
         },
     }
@@ -20,65 +22,93 @@ const domManager = (() => {
         return _references;
     };
 
-    function hideElement(element) { element.classList.add('hidden'); }
-    function unhideElement(element) { element.classList.remove('hidden'); }
-
-    function createGameboards(ratio) {
-        _createPlayerGameboards(_references.game.p1);
-        _createPlayerGameboards(_references.game.p2);
-
-        function _createPlayerGameboards(playerRef) {
-            let gameboard = document.createElement('gameboard');
-            hideElement(gameboard);
-            _references.game.sections.left.appendChild(gameboard);
-            _setElementAspectRatio(gameboard, ratio);
-            playerRef.playerBoard = gameboard;
-
-            gameboard = document.createElement('gameboard');
-            hideElement(gameboard);
-            _references.game.sections.right.appendChild(gameboard);
-            _setElementAspectRatio(gameboard, ratio);
-            playerRef.enemyBoard = gameboard;
-
-            function _setElementAspectRatio(element, ratio = 1) {
-                const parent = element.parentNode;
-                window.addEventListener('resize', _onResize);
-                _onResize();
-                function _onResize(e) {
-                    const width = parent.offsetWidth;
-                    const height = parent.offsetHeight;
-
-                    let newSize;
-                    if (height > width) newSize = width;
-                    else newSize = height;
-
-                    let newWidth = newSize;
-                    let newHeight = newSize;
-                    if (ratio > 1) newHeight *= (1 / ratio);
-                    else newWidth *= ratio;
-
-                    element.style.width = newWidth + 'px';
-                    element.style.height = newHeight + 'px';
-                }
-            }
-        }
-    };
-    function switchToPlayer1() {
-        _hidePlayerBoards(_references.game.p2);
-        _showPlayerBoards(_references.game.p1);
-    }
-    function switchToPlayer2() {
-        _hidePlayerBoards(_references.game.p1);
-        _showPlayerBoards(_references.game.p2);
-    }
-    function _hidePlayerBoards(playerRef){
-        hideElement(playerRef.playerBoard);
-        hideElement(playerRef.enemyBoard);
-    }
-    function _showPlayerBoards(playerRef){
-        unhideElement(playerRef.playerBoard);
-        unhideElement(playerRef.enemyBoard);
-    }
-    return { getReferences, hideElement, unhideElement, switchToPlayer1, switchToPlayer2, createGameboards };
+    function _hideElement(element) { element.classList.add('hidden'); }
+    function _unhideElement(element) { element.classList.remove('hidden'); }
+    return { getReferences,};
 })();
 export default domManager;
+
+// const domManager = (() => {
+//     const _references = {
+//         game: {
+//             sections: {
+//                 left: document.getElementById('left-section'),
+//                 right: document.getElementById('right-section'),
+//                 top: document.getElementById('top-section')
+//             },
+//             p1: {
+//                 playerBoard: undefined,
+//                 enemyBoard: undefined
+//             },
+//             p2: {
+//                 playerBoard: undefined,
+//                 enemyBoard: undefined
+//             },
+//         },
+//     }
+//     function getReferences() {
+//         return _references;
+//     };
+
+//     function hideElement(element) { element.classList.add('hidden'); }
+//     function unhideElement(element) { element.classList.remove('hidden'); }
+
+//     function createGameboards(ratio) {
+//         _createPlayerGameboards(_references.game.p1);
+//         _createPlayerGameboards(_references.game.p2);
+
+//         function _createPlayerGameboards(playerRef) {
+//             let gameboard = document.createElement('gameboard');
+//             hideElement(gameboard);
+//             _references.game.sections.left.appendChild(gameboard);
+//             _setElementAspectRatio(gameboard, ratio);
+//             playerRef.playerBoard = gameboard;
+
+//             gameboard = document.createElement('gameboard');
+//             hideElement(gameboard);
+//             _references.game.sections.right.appendChild(gameboard);
+//             _setElementAspectRatio(gameboard, ratio);
+//             playerRef.enemyBoard = gameboard;
+
+//             function _setElementAspectRatio(element, ratio = 1) {
+//                 const parent = element.parentNode;
+//                 window.addEventListener('resize', _onResize);
+//                 _onResize();
+//                 function _onResize(e) {
+//                     const width = parent.offsetWidth;
+//                     const height = parent.offsetHeight;
+
+//                     let newSize;
+//                     if (height > width) newSize = width;
+//                     else newSize = height;
+
+//                     let newWidth = newSize;
+//                     let newHeight = newSize;
+//                     if (ratio > 1) newHeight *= (1 / ratio);
+//                     else newWidth *= ratio;
+
+//                     element.style.width = newWidth + 'px';
+//                     element.style.height = newHeight + 'px';
+//                 }
+//             }
+//         }
+//     };
+//     function switchToPlayer1() {
+//         _hidePlayerBoards(_references.game.p2);
+//         _showPlayerBoards(_references.game.p1);
+//     }
+//     function switchToPlayer2() {
+//         _hidePlayerBoards(_references.game.p1);
+//         _showPlayerBoards(_references.game.p2);
+//     }
+//     function _hidePlayerBoards(playerRef){
+//         hideElement(playerRef.playerBoard);
+//         hideElement(playerRef.enemyBoard);
+//     }
+//     function _showPlayerBoards(playerRef){
+//         unhideElement(playerRef.playerBoard);
+//         unhideElement(playerRef.enemyBoard);
+//     }
+//     return { getReferences, hideElement, unhideElement, switchToPlayer1, switchToPlayer2, createGameboards };
+// })();
+// export default domManager;
