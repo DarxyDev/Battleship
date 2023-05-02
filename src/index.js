7e1a44168ca7a5bee80de30720a985668ac2904d
DarxyDev <damongarvin@gmail.com>
2023-05-02 18:05:26 -0400

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,11 +1,2 @@
 import './style/normalize.css';
 import './style/style.css';
-import sceneManager from './script/scene-manager';
-
-const main = (async()=>{
-    //init
-    const scenes = sceneManager.getScenes();
-    await scenes.playerSelectScene.initAsync();
-    // start
-    sceneManager.loadScene(scenes.playerSelectScene);
-})()
